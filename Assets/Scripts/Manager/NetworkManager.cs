3c5323a3acb65883b686a082d2621134a4f46ea9
vamdochi <tpghks98@naver.com>
2021-07-14 21:53:03 +0900

    fix: default skill
    - 스킬 기본값 밀기로 변경

--- Assets/Scripts/Manager/NetworkManager.cs
+++ Assets/Scripts/Manager/NetworkManager.cs
@@ -55,7 +55,7 @@ public class NetworkManager : Manager.SingletonPhoton<NetworkManager>
 
             if (!PhotonNetwork.player.TryGetValueToInt("SkillType", out dummy))
             {
-                PhotonNetwork.player.CustomProperties["SkillType"] = SkillType.SpawnVine;
+                PhotonNetwork.player.CustomProperties["SkillType"] = SkillType.PushHand;
             }
         }
         base.OnConnectedToMaster();
